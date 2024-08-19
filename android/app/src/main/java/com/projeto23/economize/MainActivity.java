a8b095895a61d210ba3789d97b8f0f3474c62890
Bernardo Simões do Nascimento <bernardosimoesdonascimento@gmail.com>
2024-08-18 22:49:37 -0300

    android

---
+++ android/app/src/main/java/com/projeto23/economize/MainActivity.java
@@ -0,0 +1,5 @@
+package com.projeto23.economize;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.projeto23.economize',
+  appName: 'economize-023',
+  webDir: 'build'
+};
+
+export default config;
